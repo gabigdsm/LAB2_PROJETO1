7150060f5312eea7bbc929fb28db7f46426941ed
Gabrielli Gonçalves da Silva <C3007896@fgv.edu.br>
2025-08-13 11:58:06 -0300

    Adicionei a media dos numeros sorteados

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print ('Hello World')
+def cumprimento(texto):
+    return f"Olá, {texto}"
+print(cumprimento("Gabrielli Gonçalves"))
+
+import random
+
+def sorteia_media():
+    numeros = [random.randint(1, 100) for _ in range(7)]
+    media = sum(numeros) / len(numeros)
+    return numeros, media
+
+numeros_sorteados, media = sorteia_media()
+print('Números sorteados:', numeros_sorteados)
+print(f'Média dos 7 números sorteados: {media:.2f}')
